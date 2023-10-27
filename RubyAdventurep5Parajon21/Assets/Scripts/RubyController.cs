0382dc36e5b6f7615a1412b82cbd2357c0e16543
Salvador Parajon <723596@student.dusd.net>
2023-10-27 13:50:43 -0700

    Ruby Moves up now
    I made Ruby have vertical movement

--- RubyAdventurep5Parajon21/Assets/Scripts/RubyController.cs
+++ RubyAdventurep5Parajon21/Assets/Scripts/RubyController.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Start is called before the first frame update
     void Start()
     {
-        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;
     }
-
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + -0.1f;
+        position.x = position.x + 3f * horizontal * Time.deltaTime;
+        position.y = position.y + 3f * vertical * Time.deltaTime;
         transform.position = position;
+       
     }
+    
+
+    
+
+
 }
