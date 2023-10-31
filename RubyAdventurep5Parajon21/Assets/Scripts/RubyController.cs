3afe93c7089e36eb36cea58bc39e00084f71aaf3
Salvador Parajon <723596@student.dusd.net>
2023-10-31 13:50:43 -0700

    World was made
    I made the tile and map at least for now it looks fine

--- RubyAdventurep5Parajon21/Assets/Scripts/RubyController.cs
+++ RubyAdventurep5Parajon21/Assets/Scripts/RubyController.cs
@@ -10,7 +10,7 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         QualitySettings.vSyncCount = 0;
-        Application.targetFrameRate = 10;
+        Application.targetFrameRate = 60;
     }
     // Update is called once per frame
     void Update()
