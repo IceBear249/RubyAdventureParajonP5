82a316b1ca3f14b5f13a557f741b4cac8eed3212
Salvador Parajon <723596@student.dusd.net>
2023-11-08 13:48:18 -0800

    Enemy
    Made an enemy that can damage you but I need to fix his rotation

--- RubyAdventurep5Parajon21/Assets/Scripts/RubyController.cs
+++ RubyAdventurep5Parajon21/Assets/Scripts/RubyController.cs
@@ -9,13 +9,18 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
 
-    public int health { get { return currentHealth; } }
+    public int health {  get { return currentHealth; } }
     int currentHealth;
-    
+
+    bool isInvincible;
+    float invincibleTimer;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+   
 
     //Start is called before the first frame update
     void Start()
@@ -32,6 +37,13 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        if (isInvincible )
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0 )
+                isInvincible = false;
+        }
+
     }
     void FixedUpdate()
     {
@@ -45,6 +57,14 @@ public class RubyController : MonoBehaviour
      
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
