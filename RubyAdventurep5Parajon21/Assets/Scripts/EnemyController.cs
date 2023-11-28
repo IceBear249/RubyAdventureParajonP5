10a586fbde6e0f53e4bc9e742725e5754227de1f
Salvador Parajon <723596@student.dusd.net>
2023-11-27 16:56:37 -0800

    Added animations
    Animations have been added to the enemies

--- RubyAdventurep5Parajon21/Assets/Scripts/EnemyController.cs
+++ RubyAdventurep5Parajon21/Assets/Scripts/EnemyController.cs
@@ -12,13 +12,15 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
-    
+
 
     // Start is called before the first frame update
+    Animator animator;
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -38,10 +40,14 @@ public class EnemyController : MonoBehaviour
 
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction; ;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction; ;
         }
         
